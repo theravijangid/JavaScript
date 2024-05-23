dba1d4e19d9dc18b5019bc5bd4e2c7f826688f33
RaviJangir <ravijangir993@gmail.com>
2024-05-23 19:13:01 +0530

    setup

---
+++ 01_basics/01.js
@@ -0,0 +1,7 @@
+for(let i=0 ; i <= 6 ; i++) {
+    let line = ''
+    for(let j=1; j < i; j++) {
+        line +=j
+    }
+    console.log(line)
+}
---
+++ 01_basics/02.js
@@ -0,0 +1,15 @@
+//data types
+
+let name = "ravi"          //string
+let age = 24               //number => 2 to power 53
+letisloggedIn = false      
+//bigint
+//null => standalone value
+//undefined
+//symbol => unique
+
+
+
+//objects
+
+console.log(typeof(name))
