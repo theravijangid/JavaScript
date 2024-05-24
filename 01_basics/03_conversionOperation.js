36ee7c0ea14792665c7f413758aec14cdbcbe997
RaviJangir <ravijangir993@gmail.com>
2024-05-24 21:58:03 +0530

    02

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score = "33abc"
+
+console.log(typeof score)
+
+const valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+// *************************************OPERATIONS***************************
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = " ravi"
+let concat = str1 + str2
+console.log(concat)
+
