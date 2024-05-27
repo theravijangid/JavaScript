7d75e7d66b01cfb5483e2e928fb731d1f89a008e
RaviJangir <ravijangir993@gmail.com>
2024-05-27 22:20:36 +0530

    d05

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,10 +6,50 @@ if (true) {
     //block scope
     let a = 10
     const b= 20
-    console.log("INNER: ",a)
+    // console.log("INNER: ",a)
 }
 
 
-console.log(a)
+// console.log(a)
 // console.log(b)
 // console.log(c)
+
+
+//Nested scope
+function one() {
+    const username = "ravi"
+    function two() {
+        const website ="youtube"
+        console.log(username)
+    }
+    // console.log(website)
+    two()
+}
+
+// one()
+
+
+if(true) {
+    const username = "ravi"
+    if(username === "ravi") {
+        const website = " youtube"
+        // console.log(username + website)
+    }
+    // console.log(website)
+}
+// console.log(username)
+
+
+//+++++++++++++++++++++++++++++++ interesting +++++++++++++++++++++++++
+console.log(addone(5))
+function addone(num) {
+    return num + 1
+}
+
+ 
+
+//also called as expression
+const addtwo = function(num) {
+    return num + 2
+}
+addtwo(5)
