84398ebb3d3f69c184acf087723556377da918a9
RaviJangir <ravijangir993@gmail.com>
2024-05-28 23:21:14 +0530

    d06

---
+++ 05_iterations/02.js
@@ -0,0 +1,20 @@
+
+let i = 0 
+while (i <= 10) {
+    //console.log(`Value of index: ${i}`)
+    i +=2
+}
+
+
+let myArr = ["iron man", "hulk", "thor"]
+let arr = 0
+while(arr < myArr.length) {
+    //console.log(`Value is ${myArr[arr]}`)
+    arr +=1
+}
+
+let score = 11
+do {
+    console.log(`Score is ${score}`)
+    score++
+} while (score <= 10);
